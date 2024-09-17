60c386a60d61ad7ba950422e1fde914890e3411f
daniell-olaitan <daniell.olaitan@gmail.com>
2024-09-17 23:54:56 +0100

    Create a module that implement password encryption

---
+++ 0x00-personal_data/encrypt_password.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+Implement a function that perform password encryption
+"""
+import bcrypt
+
+
+def hash_password(password: str) -> bytes:
+    """
+    Hash a given password
+    """
+    password = password.encode('uft-8')
+
+    return bcrypt.hashpw(password, bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """
+    Verify the validity of a password
+    """
+    return bcrypt.checkpw(password, hashed_password)
